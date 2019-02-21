c85a05a4c469959cbec559e2e0ea9febeed05e4e
Adam Mathieson <mathieson1222@gmail.com>
2019-02-20 22:08:18 -0800

    added commponentDidMount function to App.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -11,9 +11,12 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      posts: dummyData
+      posts: []
     };
   }
+  componentDidMount() {
+    this.setState({ posts: dummyData })
+}
 
   render() {
     return (
